c509642ad7937b84a3611bff86f2871899fe7a06
JorgeRea <danireaprado@hotmail.com>
2024-09-12 16:33:03 -0600

    CORS instalados

--- run.py
+++ run.py
@@ -1,6 +1,10 @@
 from src import create_app
+from flask_cors import CORS
 
 app = create_app()
+
+# Habilitar CORS para todas las rutas
+CORS(app)
 
 if __name__ == "__main__":
     app.run(debug=True, port=8080, host='0.0.0.0')
